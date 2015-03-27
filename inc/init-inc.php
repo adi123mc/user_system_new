92eba6829020ecb8890b833c47be148a2d472cf1
adi123mc <adi123mc@gmail.com>
2015-03-27 19:49:31 +0300

    change

--- inc/init-inc.php
+++ inc/init-inc.php
@@ -9,12 +9,12 @@ $page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0 , -4);
 if (in_array($page, $exceptions) === false) {
 	if (isset($_SESSTION['username']) === false) {
 		header('Location: login.php');
-		die();
+		die(mysql_error());
 	}
 
 }
 
-mysql_connect('127.0.0.1', 'root');
+mysql_connect("127.0.0.1", "root", "");
 mysql_select_db('user_system');
 
 $path = dirname(__FILE__);
