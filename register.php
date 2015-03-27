545419568a19124e96684c5baa47dc107acc005b
Ninjoh <m.heil375@gmail.com>
2015-03-27 20:41:46 +0100

    Stuff again

--- register.php
+++ register.php
@@ -1,8 +1,14 @@
 <?php
+//Turn of deprecated and strict warnings
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+//Include global vars
+include('/inc/vars-inc.php');
+
 include('/inc/init-inc.php');
 
 echo '<h1>You wot m8?</h1>';//Fancy stuffs ;3
+echo $current_page;
 
 if (isset($_POST['username'], $_POST['password'], $_POST['repeat_password'])) {
 	if (empty($_POST['email'])) {
@@ -16,18 +22,8 @@ if (isset($_POST['username'], $_POST['password'], $_POST['repeat_password'])) {
 	if (empty($_POST['password']) || empty($_POST['repeat_password'])) {
 		$errors[] = 'the password can not be empty.';
 	}
-
-	if ($_POST['password'] !== $_POST['repeat_password']) {
-		$errors[] = 'Passwords do not match.';
-	}
-
-	if (user_exists($_POST['username'])) {
-		$errors[] = 'Username already taken.';
-	}
-
-	if (empty($errors)) {
-		
-		//Get data submitted from forms
+	
+	//Get data submitted from forms
 		$email = $_POST['email'];
 		$username = $_POST['username'];
 		$password = $_POST['password'];
@@ -53,10 +49,95 @@ if (isset($_POST['username'], $_POST['password'], $_POST['repeat_password'])) {
 		//Make email lowercase
 		$email = strtolower($email);
 		
-		//Check if any fields already exist
+		
+		//Check if email already exists in database
+		
+		//Prepare SQL
+		$sql = "SELECT * FROM `users` WHERE `email` LIKE '$email' ORDER BY `user_id` ASC";
+		
+			//Execute query
+			$result = mysql_query($sql);
+				if ($result === false) {
+					die("Could not query database");
+					}
+					
+						//Check wether we found a row
+						if (mysql_num_rows($result) == 1) {
+							$errors[] = 'Email is already in use.';
+						} elseif {
+						
+						//Check if username already exists in database
+							
+							//Prepare SQL
+							$sql = "SELECT * FROM `users` WHERE `username` LIKE '$username' ORDER BY `id` ASC";
+								
+								//Execute query
+								$result = mysql_query($sql);
+									if ($result === false) {
+										die("Could not query database");
+										}
+								
+									//Check wether we found a row
+									if (mysql_num_rows($result) == 1) {
+										$errors[] = 'Email is already in use.';
+										}
+									}		
+											//Prepare for confirmation email
+											
+											//Generate random user_key
+											$user_key = $username . $email . date("Ymd");
+											$user_key = md5($user_key);
+											
+											//Prepare SQL
+											$sql = "INSERT INTO `polimf_ninja`.`users` (`id`, `user_key`, `confirmed`, `email`, `username`, `password`) VALUES (NULL, '$user_key', '0', '$email', '$username', '$password');";
+											
+											//Execute query
+											$result = mysql_query($sql);
+												if ($result === false) {
+													die("Could not query database");
+												}
+												
+											
+											//Send confirmation email
+											$to      = "$email";
+											$subject = "Registration email for $site_name";
+											$message = "Please the following link to confirm your email \n$domain/registration_confirm.php?email=$email&user_key=$user_key \n\n\n if you did not register this account, please ignore this email.";
+											$headers = "From: $system_mail" . "\r\n" .
+												"Reply-To: $support_mail" . "\r\n";
+
+											mail($to, $subject, $message, $headers);
+	
+	
+	
+	
+	
+	
+	
+	
+	//Check if passwords match
+	if ($_POST['password'] !== $_POST['repeat_password']) {
+		$errors[] = 'Passwords do not match.';
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	if (empty($errors)) {
 		
 		
-		//Send confirmation email to user
 		die();
 	}
 }
