1a34a1610bc64df824fdd2a152174feef690f762
adi123mc <adi123mc@gmail.com>
2015-03-27 17:59:44 +0300

    added the files

---
+++ inc/init-inc.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+$exceptions = array('register' , 'login');
+
+$page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0 , -4);
+
+if (in_array($page, $exceptions) === false) {
+	if (isset($_SESSTION['username']) === false) {
+		header('Location: login.php');
+		die();
+	}
+
+}
+
+mysql_connect('127.0.0.1', 'root');
+mysql_select_db('user_system');
+
+$path = dirname(__FILE__);
+
+include("/inc/user-inc.php");
+
+?>
